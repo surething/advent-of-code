2e99698baebc8592f91cc79476c24b75a150aef8
Dan Erik Petersen <dan.erik.petersen@mac.com>
2025-11-15 15:50:47 +1300

    feat(2017): day 1

--- crates/aoc-2017/src/day1.rs
+++ crates/aoc-2017/src/day1.rs
@@ -1,6 +1,18 @@
 use aoc_common::prelude::*;
 use aoc_data::prelude::*;
 
+fn kernel(acc: u32, pair: (char, char)) -> Result<u32> {
+    let (a, b) = pair;
+    if a == b {
+        match a.to_digit(10) {
+            Some(v) => Ok(acc + v),
+            None => Err(AdventError::Other(format!("Invalid digit: {}", a))),
+        }
+    } else {
+        Ok(acc)
+    }
+}
+
 struct Solver {}
 
 impl ResourceReader for Solver {}
@@ -15,11 +27,26 @@ impl Task for Solver {
     }
 
     fn solve_part1(&self, input: &str) -> Result<String> {
-        Ok("0".to_string())
-    }
+        let trimmed = input.trim();
+        trimmed
+            .chars()
+            .chain(trimmed.chars().take(1))
+            .tuple_windows()
+            .try_fold(0, kernel)
+            .map(|sum| sum.to_string())
+      }
 
     fn solve_part2(&self, input: &str) -> Result<String> {
-        Ok("0".to_string())
+        let trimmed = input.trim();
+        let len = trimmed.len();
+        trimmed
+            .chars()
+            .cycle()
+            .skip(len / 2)
+            .take(len)
+            .zip(trimmed.chars())
+            .try_fold(0, kernel)
+            .map(|sum| sum.to_string())
     }
 }
 
@@ -35,30 +62,37 @@ mod test {
     }
 
     #[rstest]
-    fn example1(solver: Solver) -> Result<()> {
-        let input = solver.read_resource(Input::Example1)?;
-        assert_eq!(solver.solve_part1(&input)?, "0");
+    #[case("1122", "3")]
+    #[case("1111", "4")]
+    #[case("1234", "0")]
+    #[case("91212129", "9")]
+    fn example1(solver: Solver, #[case] input: String, #[case] expected: String) -> Result<()> {
+        assert_eq!(solver.solve_part1(&input)?, expected);
         Ok(())
     }
 
     #[rstest]
-    fn example2(solver: Solver) -> Result<()> {
-        let input = solver.read_resource(Input::Example2)?;
-        assert_eq!(solver.solve_part2(&input)?, "0");
+    #[case("1212", "6")]
+    #[case("1221", "0")]
+    #[case("123425", "4")]
+    #[case("123123", "12")]
+    #[case("12131415", "4")]
+    fn example2(solver: Solver, #[case] input: String, #[case] expected: String) -> Result<()> {
+        assert_eq!(solver.solve_part2(&input)?, expected);
         Ok(())
     }
 
     #[rstest]
     fn part1(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Part1)?;
-        assert_eq!(solver.solve_part1(&input)?, "0");
+        assert_eq!(solver.solve_part1(&input)?, "1228");
         Ok(())
     }
 
     #[rstest]
     fn part2(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Part2)?;
-        assert_eq!(solver.solve_part2(&input)?, "0");
+        assert_eq!(solver.solve_part2(&input)?, "1238");
         Ok(())
     }
 }
