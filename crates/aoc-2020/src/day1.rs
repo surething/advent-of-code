f05cbf72f2c435d4b5049c30bd6de36d41493d3e
Dan Erik Petersen <dan.erik.petersen@mac.com>
2025-11-15 18:12:38 +1300

    feat(2020): day 1

--- crates/aoc-2020/src/day1.rs
+++ crates/aoc-2020/src/day1.rs
@@ -1,5 +1,21 @@
 use aoc_common::prelude::*;
 use aoc_data::prelude::*;
+use nom::character::complete;
+use nom::character::complete::newline;
+use nom::combinator::opt;
+use nom::multi::many1;
+use nom::sequence::terminated;
+
+fn parse_expense(i: &str) -> IResult<&str, i32> {
+    terminated(
+        complete::i32,
+        opt(newline),
+    ).parse(i)
+}
+
+fn parse_input(i: &str) -> Result<Vec<i32>> {
+    many1(parse_expense).parse(i).map_and_finish()
+}
 
 struct Solver {}
 
@@ -15,11 +31,23 @@ impl Task for Solver {
     }
 
     fn solve_part1(&self, input: &str) -> Result<String> {
-        Ok("0".to_string())
+        let expenses = parse_input(input.trim())?;
+        expenses
+            .into_iter()
+            .combinations(2)
+            .find(|c| c.iter().sum::<i32>() == 2020)
+            .map(|c| c.iter().product::<i32>().to_string())
+            .ok_or(AdventError::Other("No valid pair found".to_string()))
     }
 
     fn solve_part2(&self, input: &str) -> Result<String> {
-        Ok("0".to_string())
+        let expenses = parse_input(input.trim())?;
+        expenses
+            .into_iter()
+            .combinations(3)
+            .find(|c| c.iter().sum::<i32>() == 2020)
+            .map(|c| c.iter().product::<i32>().to_string())
+            .ok_or(AdventError::Other("No valid triplet found".to_string()))
     }
 }
 
@@ -37,28 +65,28 @@ mod test {
     #[rstest]
     fn example1(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Example1)?;
-        assert_eq!(solver.solve_part1(&input)?, "0");
+        assert_eq!(solver.solve_part1(&input)?, "514579");
         Ok(())
     }
 
     #[rstest]
     fn example2(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Example2)?;
-        assert_eq!(solver.solve_part2(&input)?, "0");
+        assert_eq!(solver.solve_part2(&input)?, "241861950");
         Ok(())
     }
 
     #[rstest]
     fn part1(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Part1)?;
-        assert_eq!(solver.solve_part1(&input)?, "0");
+        assert_eq!(solver.solve_part1(&input)?, "864864");
         Ok(())
     }
 
     #[rstest]
     fn part2(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Part2)?;
-        assert_eq!(solver.solve_part2(&input)?, "0");
+        assert_eq!(solver.solve_part2(&input)?, "281473080");
         Ok(())
     }
 }
