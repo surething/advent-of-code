3fd508b95e216b2f1e77c745a112ead3bb41671b
Dan Erik Petersen <dan.erik.petersen@mac.com>
2025-11-16 14:40:33 +1300

    chore: stubs

---
+++ crates/aoc-2015/src/day4.rs
@@ -0,0 +1,64 @@
+use aoc_common::prelude::*;
+use aoc_data::prelude::*;
+
+struct Solver {}
+
+impl ResourceReader for Solver {}
+
+impl Task for Solver {
+    fn event(&self) -> Event {
+        Event::Event2015
+    }
+
+    fn day(&self) -> Day {
+        Day::Day4
+    }
+
+    fn solve_part1(&self, input: &str) -> Result<String> {
+        Ok("0".to_string())
+    }
+
+    fn solve_part2(&self, input: &str) -> Result<String> {
+        Ok("0".to_string())
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use rstest::*;
+
+    #[fixture]
+    fn solver() -> Solver {
+        Solver {}
+    }
+
+    #[rstest]
+    fn example1(solver: Solver) -> Result<()> {
+        let input = solver.read_resource(Input::Example1)?;
+        assert_eq!(solver.solve_part1(&input)?, "0");
+        Ok(())
+    }
+
+    #[rstest]
+    fn example2(solver: Solver) -> Result<()> {
+        let input = solver.read_resource(Input::Example2)?;
+        assert_eq!(solver.solve_part2(&input)?, "0");
+        Ok(())
+    }
+
+    #[rstest]
+    fn part1(solver: Solver) -> Result<()> {
+        let input = solver.read_resource(Input::Part1)?;
+        assert_eq!(solver.solve_part1(&input)?, "0");
+        Ok(())
+    }
+
+    #[rstest]
+    fn part2(solver: Solver) -> Result<()> {
+        let input = solver.read_resource(Input::Part2)?;
+        assert_eq!(solver.solve_part2(&input)?, "0");
+        Ok(())
+    }
+}
--- crates/aoc-2025/src/lib.rs
+++ crates/aoc-2025/src/lib.rs
@@ -1,1 +1,14 @@
+#![allow(unused)]
+
 mod day1;
+mod day10;
+mod day11;
+mod day12;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
