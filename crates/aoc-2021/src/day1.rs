ec063bf3bf31ea43f3986cf0a8bdc7569c780ee7
Dan Erik Petersen <dan.erik.petersen@mac.com>
2025-11-16 13:04:29 +1300

    feat(2021): day 1

--- crates/aoc-2021/src/day1.rs
+++ crates/aoc-2021/src/day1.rs
@@ -1,5 +1,21 @@
+use nom::character::complete;
+use nom::combinator::opt;
+use nom::multi::many1;
+use nom::sequence::terminated;
 use aoc_common::prelude::*;
 use aoc_data::prelude::*;
+
+fn parse_depth(i: &str) -> IResult<&str, i32> {
+    terminated(
+        complete::i32,
+        opt(complete::newline),
+    )
+    .parse(i)
+}
+
+fn parse_input(i: &str) -> Result<Vec<i32>> {
+    many1(parse_depth).parse(i).map_and_finish()
+}
 
 struct Solver {}
 
@@ -15,11 +31,25 @@ impl Task for Solver {
     }
 
     fn solve_part1(&self, input: &str) -> Result<String> {
-        Ok("0".to_string())
+        let depths = parse_input(input.trim())?;
+        let count = depths
+            .iter()
+            .tuple_windows()
+            .filter(|(a, b)| b > a)
+            .count();
+        Ok(count.to_string())
     }
 
     fn solve_part2(&self, input: &str) -> Result<String> {
-        Ok("0".to_string())
+        let depths = parse_input(input.trim())?;
+        let count = depths
+            .iter()
+            .tuple_windows()
+            .map(|(a, b, c)| a + b + c)
+            .tuple_windows()
+            .filter(|(a, b)| b > a)
+            .count();
+        Ok(count.to_string())
     }
 }
 
@@ -37,28 +67,28 @@ mod test {
     #[rstest]
     fn example1(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Example1)?;
-        assert_eq!(solver.solve_part1(&input)?, "0");
+        assert_eq!(solver.solve_part1(&input)?, "7");
         Ok(())
     }
 
     #[rstest]
     fn example2(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Example2)?;
-        assert_eq!(solver.solve_part2(&input)?, "0");
+        assert_eq!(solver.solve_part2(&input)?, "5");
         Ok(())
     }
 
     #[rstest]
     fn part1(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Part1)?;
-        assert_eq!(solver.solve_part1(&input)?, "0");
+        assert_eq!(solver.solve_part1(&input)?, "1374");
         Ok(())
     }
 
     #[rstest]
     fn part2(solver: Solver) -> Result<()> {
         let input = solver.read_resource(Input::Part2)?;
-        assert_eq!(solver.solve_part2(&input)?, "0");
+        assert_eq!(solver.solve_part2(&input)?, "1418");
         Ok(())
     }
 }
